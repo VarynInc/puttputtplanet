ae728b437bf5791fb0802a557f133d07e9bd7836
John Foster <john@varyn.com>
2020-12-12 15:08:23 -0800

    sync libs

--- services/Enginesis.php
+++ services/Enginesis.php
@@ -6,7 +6,7 @@
  */
 
 if ( ! defined('ENGINESIS_VERSION')) {
-    define('ENGINESIS_VERSION', '2.6.2');
+    define('ENGINESIS_VERSION', '2.6.6');
 }
 require_once('EnginesisErrors.php');
 if ( ! defined('SESSION_COOKIE')) {
@@ -605,6 +605,7 @@ class Enginesis {
         }
         $this->m_serviceHost = $enginesisServiceHost;
         $this->m_serviceProtocol = $protocol;
+        $this->debugInfo("Using host $protocol $enginesisServiceHost", __FILE__, __LINE__);
         return $this->m_serviceHost;
     }
 
@@ -1321,6 +1322,7 @@ class Enginesis {
                 $serverParams['language_code'] = $this->m_languageCode;
             }
         } else {
+            $serverParams['fn'] = $serviceName;
             $serverParams['site_id'] = $this->m_siteId;
             $serverParams['user_id'] = $this->m_userId;
             $serverParams['response'] = $this->m_responseFormat;
@@ -1379,7 +1381,7 @@ class Enginesis {
         $isLocalhost = serverStage() == '-l';
         $url = $this->m_serviceEndPoint;
         $setSSLCertificate = startsWith(strtolower($url), 'https://');
-        $this->debugInfo("Calling $serviceName with " . json_encode($parameters), __FILE__, __LINE__);
+        $this->debugInfo("Calling $serviceName on $url with " . json_encode($parameters), __FILE__, __LINE__);
         $ch = curl_init($url);
         if ($ch) {
             $referrer = $this->m_serviceProtocol . '://' . $this->getServerName() . $this->currentPagePath();
@@ -1622,7 +1624,7 @@ class Enginesis {
         }
         // When refreshing the token we need to remind the server who the user is
         $parameters = [
-            'authtok' => $refreshToken,
+            'refresh_token' => $refreshToken,
             'logged_in_user_id' => $this->m_userId,
             'game_id' => $this->m_gameId,
             'gamekey' => gameKeyMake($this->m_siteId, $this->m_gameId)
@@ -2135,15 +2137,18 @@ class Enginesis {
 
     /**
      * If the secondary password expires or the user lost it, we come here to generate a new one and send a new email.
-     * site-id, user-id, and previous token must match otherwise generates INVALID_USER_ID error. At least two
-     * of the parameters must be provided to identify the user.
-     * @param $userId
-     * @param $secondaryPassword
-     * @return bool
+     * site-id, one of [userId, userName, email] to identify the user, and previous token must match otherwise
+     * returns INVALID_USER_ID error.
+     *
+     * @param integer $userId
+     * @param string $userName
+     * @param string $email
+     * @param string $secondaryPassword
+     * @return boolean
      */
-    public function registeredUserResetSecondaryPassword ($userId, $secondaryPassword) {
+    public function registeredUserResetSecondaryPassword ($userId, $userName, $email, $secondaryPassword) {
         $service = 'RegisteredUserResetSecondaryPassword';
-        $parameters = ['user_id' => $userId, 'secondary_password' => $secondaryPassword];
+        $parameters = ['user_id' => $userId, 'user_name' => $userName, 'email' => $email, 'secondary_password' => $secondaryPassword];
         $enginesisResponse = $this->callServerAPI($service, $parameters);
         return $this->setLastErrorFromResponse($enginesisResponse);
     }
