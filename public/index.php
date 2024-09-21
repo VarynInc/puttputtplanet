340f0592d5cf3ba761041420c7126c7cacfe2d81
John Foster <jfoster@acm.org>
2024-09-21 16:41:19 -0700

    minor fixes; version up 1.0.9

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 require_once '../services/common.php';
 ?>
 <!doctype html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -11,11 +11,11 @@ require_once '../services/common.php';
     <link rel="manifest" href="site.webmanifest">
     <link rel="icon" type="image/png" href="icon-32.png">
     <link rel="apple-touch-icon" href="icon.png">
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <META NAME="Description" CONTENT="Putt Putt Planet is the mini golf destination on the internet">
     <META NAME="Keywords" CONTENT="mini golf minigolf free online games enginesis varyn download PC internet web development entertainment">
     <META NAME="Author" content="Varyn, Inc.">
-    <META NAME="Copyright" content="Copyright © 2010 Varyn, Inc. All rights reserved.">
+    <META NAME="Copyright" content="Copyright 2010 Varyn, Inc. All rights reserved.">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet" type="text/css">
     <script type="application/ld+json">
@@ -40,7 +40,7 @@ require_once '../services/common.php';
     <div class="container top-promo-area">
         <div class="row justify-content-center">
             <div id="hero-image" class="col-6">
-                <img class="img-fluid" src="images/puttputt-logo01.png"/>
+                <img class="img-fluid" src="images/puttputt-logo01.png" alt="Putt putt planet world view planet image with logo">
             </div>
         </div>
         <div class="row text-center">
@@ -50,7 +50,7 @@ require_once '../services/common.php';
         <div class="row justify-content-center">
             <div class="col-2 text-center">
                 <a href="https://www.varyn.com" target="_blank" title="Varyn - game development, game technology, game consulting services">
-                <img class="varyn-logo-footer" src="images/Varyn-Logo-250x150.png" alt="Varyn, Inc." />
+                <img class="varyn-logo-footer" src="images/Varyn-Logo-250x150.png" alt="Varyn, Inc. dragon wing logo">
                 </a><br>
                 <span class="poweredby">Developed by Varyn</span>
             </div>
@@ -59,7 +59,7 @@ require_once '../services/common.php';
             </div>
             <div class="col-2 text-center">
                 <a href="https://www.enginesis.com" target="_blank" title="Enginesis - online games platform encompassing all back-end services needed for any online game">
-                <img src="images/EcolorLogoMicro.png" width="28" height="37" alt="Enginesis Online Games Platform" />
+                <img src="images/EcolorLogoMicro.png" width="28" height="37" alt="Enginesis online games platform logo">
                 </a><br>
                 <span class="poweredby">Powered by Enginesis</span>
             </div>
